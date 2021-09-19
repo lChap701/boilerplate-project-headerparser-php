ca8c827a23aa9fd22065ddc54449cd48f2cf3780
lChap701 <chapmanlucas201@gmail.com>
2021-09-19 11:34:46 -0500

    Removed escape characters from JSON

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ $part = explode("api/", $_SERVER["REQUEST_URI"]);
 if ($_SERVER["REQUEST_URI"] == "/" || $_SERVER["REQUEST_URI"] == "") {
   require __DIR__ . "/views/index.html";
 } else if ($_SERVER["REQUEST_URI"] == "/api/whoami" || $_SERVER["REQUEST_URI"] == "/api/whoami/") {
-  echo json_encode((object)[
-    "ipaddress" => $_SERVER["HTTP_X_FORWARDED_FOR"],
-    "language" => $_SERVER["HTTP_ACCEPT_LANGUAGE"],
-    "software" => $_SERVER["HTTP_USER_AGENT"]
-  ]);
+  echo stripcslashes(
+    json_encode((object)[
+      "ipaddress" => $_SERVER["HTTP_X_FORWARDED_FOR"],
+      "language" => $_SERVER["HTTP_ACCEPT_LANGUAGE"],
+      "software" => $_SERVER["HTTP_USER_AGENT"]
+    ])
+  );
 } else {
   echo "Cannot GET {$_SERVER["REQUEST_URI"]}";
 }
